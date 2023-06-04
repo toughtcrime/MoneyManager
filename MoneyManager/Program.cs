dde1fc6503f6630191c384f42344a07bcc85dae7
toughtcrime <vladyslavhlovatskyi@gmail.com>
2023-06-04 15:02:28 +0200

    Removed swagger

--- MoneyManager/Program.cs
+++ MoneyManager/Program.cs
@@ -1,25 +1,28 @@
-var builder = WebApplication.CreateBuilder(args);
+using Infrastructure;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddInfrastructureServices(builder.Configuration);
+        var app = builder.Build();
+        
+        if (app.Environment.IsDevelopment())
+        {
+           
+        }
+        else
+        {
+            app.UseHsts();
+        }
+        
+        app.UseHttpsRedirection();
+        app.UseAuthentication();
+        app.UseAuthorization();
+        app.MapControllers();
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
